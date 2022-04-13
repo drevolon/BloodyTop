5c97c7702c02345cd643dca2da64c8197d918e38
Kigl <kigl@yandex.ru>
2022-04-13 20:10:34 +0500

    Убрал настройки с экрана запуска

--- Assets/Code/Controllers/MainMenuController.cs
+++ Assets/Code/Controllers/MainMenuController.cs
@@ -29,13 +29,13 @@ public class MainMenuController : BaseController
 
     public void StartGame()
     {
-        string v = GameObject.Find("InputFieldVelocity").GetComponent<TMP_InputField>().text;
-        string dv = GameObject.Find("InputFieldDeltaVelocity").GetComponent<TMP_InputField>().text;
-        string w = GameObject.Find("InputFieldOmega").GetComponent<TMP_InputField>().text;
+        //string v = GameObject.Find("InputFieldVelocity").GetComponent<TMP_InputField>().text;
+        //string dv = GameObject.Find("InputFieldDeltaVelocity").GetComponent<TMP_InputField>().text;
+        //string w = GameObject.Find("InputFieldOmega").GetComponent<TMP_InputField>().text;
 
-        float.TryParse(v, out PlayerParams.StartVelocity);
-        float.TryParse(dv, out PlayerParams.DeltaVelocity);
-        float.TryParse(w,out PlayerParams.StartOmega);
+        //float.TryParse(v, out PlayerParams.StartVelocity);
+        //float.TryParse(dv, out PlayerParams.DeltaVelocity);
+        //float.TryParse(w,out PlayerParams.StartOmega);
 
         _profilePlayer.CurrentState.Value = GameState.Game;
     }
